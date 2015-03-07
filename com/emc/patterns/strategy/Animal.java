d18839a90850edb11ba4b6fdc28d9f7873b1f8bf
ecarino <emcarino@yahoo.com>
2015-03-07 10:07:35 -0500

    Clean-up code.

--- com/emc/patterns/strategy/Animal.java
+++ com/emc/patterns/strategy/Animal.java
@@ -4,15 +4,15 @@ class Animal {
 
     private Behavior behavior;
 
-    Animal() {
+    public Animal() {
         super();
     }
 
-    void setBehavior(Behavior behavior) {
+    public void setBehavior(Behavior behavior) {
         this.behavior = behavior;
     }
 
-    void showBehavior() {
+    public void showBehavior() {
         behavior.showBehavior();
     }
 
